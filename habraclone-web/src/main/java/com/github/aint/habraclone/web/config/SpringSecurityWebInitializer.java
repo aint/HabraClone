2fd7e4a126a114226ea647e7f18527226ca5b7f8
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2016-09-10 18:36:00 +0300

    Use more concise AnnotationConfigWebApplicationContext for web configuration

---
+++ habraclone-web/src/main/java/com/github/aint/habraclone/web/config/SpringSecurityWebInitializer.java
@@ -0,0 +1,6 @@
+package com.github.aint.habraclone.web.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+}
--- habraclone-web/src/main/java/com/github/aint/habraclone/web/config/WebSpringConfig.java
+++ habraclone-web/src/main/java/com/github/aint/habraclone/web/config/WebSpringConfig.java
@@ -2,47 +2,36 @@ package com.github.aint.habraclone.web.config;
 
 import com.github.aint.habraclone.data.config.DataSpringConfig;
 import com.github.aint.habraclone.service.config.ServiceSpringConfig;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.filter.DelegatingFilterProxy;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+import javax.servlet.Filter;
 import javax.servlet.ServletRegistration;
-import java.util.EnumSet;
 
-public class WebSpringConfig implements WebApplicationInitializer {
+public class WebSpringConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
-    public void onStartup(ServletContext container) throws ServletException {
-        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.register(DataSpringConfig.class, ServiceSpringConfig.class, SecuritySpringConfig.class);
-        container.addListener(new ContextLoaderListener(rootContext));
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { DataSpringConfig.class, ServiceSpringConfig.class, SecuritySpringConfig.class };
+    }
 
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { DispatcherSpringConfig.class };
+    }
 
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
 
-        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
-        dispatcherContext.register(DispatcherSpringConfig.class);
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[] { new CharacterEncodingFilter("UTF-8", true) };
+    }
 
-        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
-        dispatcher.setInitParameter("throwExceptionIfNoHandlerFound", "true");
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
-
-
-
-        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
-
-        FilterRegistration.Dynamic characterEncoding = container.addFilter("encodingFilter", new CharacterEncodingFilter("UTF-8", true));
-        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
-
-        FilterRegistration.Dynamic security = container.addFilter("springSecurityFilterChain", new DelegatingFilterProxy());
-        security.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
-
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
     }
 }
