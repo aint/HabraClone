a513900323ad3ccfd6800a1b74284d66fe9302a3
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2016-09-11 21:07:07 +0300

    Set OpenSessionInViewFilter as first filter, before springSecurityFilter
    This fix LazyInitializationException of principal's roles after successful login

--- habraclone-web/src/main/java/com/github/aint/habraclone/web/config/SpringSecurityWebInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.github.aint.habraclone.web.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
-}
